03c99ff5fbada48c620bf82111f55529598eb8b8
inmyblue <98289614+inmyblue@users.noreply.github.com>
2022-04-15 21:18:34 +0900

    프로젝트 구조 변경

--- routes/users.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const userCtl = require('../controllers/user');
-
-router.post('/signup', userCtl.signup);
-router.get('/', userCtl.find);
-
-module.exports = router;
