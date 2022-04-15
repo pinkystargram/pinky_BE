845283c5e4b83f9b5458bfd849a6f8a6d226e7be
MoingXTwice <sckbs1314@gmail.com>
2022-04-15 15:28:43 +0900

    initial commit

---
+++ app.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const morgan = require('morgan');
+
+app.use(cors());
+app.use(morgan('dev'));
+
+const swaggerUi = require('swagger-ui-express');
+const swaggerFile = require('./swagger-output');
+
+app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerFile));
+
+app.use(express.json());
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.disable('x-powered-by');
+
+const Router = require('./routes');
+app.use('/api', Router);
+
+app.use((req, res, next) => {
+    res.status(404).send('요청하신 페이지를 찾을 수 없습니다.');
+});
+
+module.exports = app;
