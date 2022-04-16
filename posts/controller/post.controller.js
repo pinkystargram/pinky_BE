c3b233845dc525a9afcf73cb6990fa156b32ee87
MoingXTwice <sckbs1314@gmail.com>
2022-04-16 21:39:34 +0900

    [add] user controller service 게시글 조회 기능 추가

--- posts/controller/post.controller.js
+++ posts/controller/post.controller.js
@@ -2,12 +2,11 @@ const postService = require('../service/post.service');
 
 module.exports = {
     post: async (req, res) => {
-        const { content } = req.body;
+        const { content, location } = req.body;
         const image = req.file.location;
-        console.log('postimage', req.file);
-        const userId = '9f393f58-e90d-4ff1-8e79-a8222cca1c01';
+        const userId = '3d5ae106-1c0b-4ea0-8df1-0fb7229c07c0';
         try {
-            await postService.createPost(userId, content, image);
+            await postService.createPost(userId, content, image, location);
             res.status(201).json({
                 result: true,
                 data: [
@@ -42,6 +41,21 @@ module.exports = {
             res.status(400).json({
                 result: false,
                 message: '게시글 작성 중 오류가 발생하였습니다.',
+            });
+        }
+    },
+    viewPost: async (req, res) => {
+        const userId = '3d5ae106-1c0b-4ea0-8df1-0fb7229c07c0';
+        console.log('find', await postService.findPost(userId));
+        try {
+            const data = await postService.findPost(userId);
+            console.log(data);
+            res.status(200).json(data);
+        } catch (error) {
+            console.log(error);
+            res.status(400).json({
+                result: false,
+                message: '게시글 조회 중 오류가 발생하였습니다.',
             });
         }
     },
