997286f8c45717781dc2777877b5d97d4a21bcc8
inmyblue <98289614+inmyblue@users.noreply.github.com>
2022-04-16 13:47:03 +0900

    auth.middleware추가

--- users/index.js
+++ users/index.js
@@ -3,8 +3,10 @@ const router = express.Router();
 
 const userController = require('./controller/user.controller');
 const registerValidator = require('./validator/register.validator');
+const authMiddleware = require('../middlewares/auth.middleware');
 
 router.post('/signup', registerValidator, userController.signup);
 router.post('/login', userController.login);
+router.get('/auth', authMiddleware.auth, userController.auth);
 
 module.exports = router;
