df195bba546f5d988f383ed5603b8186c783946a
MoingXTwice <sckbs1314@gmail.com>
2022-04-16 21:39:09 +0900

    [fix] user index.js 게시글조회 router 추가

--- posts/index.js
+++ posts/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const upload = require('../modules/multer');
+const upload = require('../middlewares/multer');
 
 const postController = require('./controller/post.controller');
 
 router.post('/', upload.single('image'), postController.post);
+router.get('/', postController.viewPost);
 
 module.exports = router;
