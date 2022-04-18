50c1c158219fef933b2cc076b56f4f96681d8e1e
pooreumu <pooreum83@gmail.com>
2022-04-18 09:06:57 +0900

    [add] 좋아요, 북마크 라우터 추가

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,13 @@ const router = require('express').Router();
 const user = require('../users');
 const post = require('../posts');
 const comment = require('../comments');
+const like = require('../likes');
+const bookmark = require('../bookmarks');
 
 router.use('/users', user);
 router.use('/posts', post);
 router.use('/comments', comment);
+router.use('/likes', like);
+router.use('/bookmarks', bookmark);
 
 module.exports = router;
