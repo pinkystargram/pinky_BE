47213d2100df5f42def5414185c31e8caf7a8f25
MoingXTwice <sckbs1314@gmail.com>
2022-04-18 10:05:17 +0900

    [feat] post validator 틀 추가

---
+++ posts/validator/post.validator.js
@@ -0,0 +1,32 @@
+const { body, validationResult } = require('express-validator');
+
+const error = (req, res, next) => {
+    const errors = validationResult(req);
+    console.log('validateError : ', errors['errors']);
+    if (errors.isEmpty()) {
+        return next();
+    }
+    console.log(errors.array()[0].msg);
+    return res.json({ result: 'fail', errorMessage: errors.array()[0].msg });
+};
+
+const postValidation = [
+    body('image')
+        .trim()
+        .isLength({ min: 5 })
+        .withMessage('아이디는 최소 5글자 이상이어야 합니다')
+        .isEmail()
+        .withMessage('아이디는 이메일 형식이어야 합니다'),
+    body('content')
+        .trim()
+        .isLength({ min: 5 })
+        .withMessage('비밀번호는 최소 5글자 이상이어야 합니다')
+        .isAlphanumeric()
+        .withMessage('비밀번호는 영문자와 숫자만 사용가능합니다'),
+    body('location')
+        .isLength({ min: 3 })
+        .withMessage('닉네임은 최소 3글자 이상이어야 합니다'),
+    error,
+];
+
+module.exports = postValidation;
